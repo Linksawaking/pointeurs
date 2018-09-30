768c8cf0676b0a18d2342e00cba6386658d2734d
Julien Benatar <julien.benatar@telecom-paristech.fr>
2018-09-30 18:30:16 +0200

    ajout fonctions sur les chaines de caractères

--- src/pointeurs.c
+++ src/pointeurs.c
@@ -10,8 +10,35 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+char* strcat2(char* s, char* t);
+char* strcpy2(char* de, char* vers);
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	char str1[50] = "!!!Hello World!!!"; //17
+	char str2[] = " Good to see you !"; //18
+
+	puts(strcat2(str1, str2));
+	//puts(strlen(str1));
 	return EXIT_SUCCESS;
+}
+
+// copie la chaine de dans la chaine vers, renvoie de
+char* strcpy2(char* de, char* vers)
+{
+	while((*vers++=*de++) != '\0')
+		;
+
+	return de;
+}
+
+char* strcat2(char* s, char* t)
+{
+	char* ps = s;
+	for(;*ps != '\0';ps++)
+		;
+	strcpy2(t, ps);
+
+	return s;
 }
